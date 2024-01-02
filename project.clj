f183a0726d12dee20123337991000c11f119add0
Bill Robertson <billrobertson42@gmail.com>
2024-01-01 23:23:51 -0500

    update to 0.21.0

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject derby2pg "0.1.0"
+(defproject derby2pg "0.21.0"
   :description "Tool to jumpstart Apache Derby to Postgres data transfer"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/billrobertson42/derby2pg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [funcool/clojure.jdbc "0.9.0"]
-                 [org.apache.derby/derby "10.13.1.1"]
+                 [org.apache.derby/derby "10.17.1.0"]
+                 [org.apache.derby/derbyclient "10.17.1.0"]
                  ]
   :profiles {:dev {:dependencies [[ring/ring-devel "1.4.0"]]}}
   :main derby2pg.core
